71cc461b2b056aefdec45d700c4531dbaae00022
Nick Hansen <nick.hansen13@gmail.com>
2019-02-28 15:17:26 -0600

    removed cave.css, empty file

--- src/components/Cave.js
+++ src/components/Cave.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import ApplicationViews from "./ApplicationViews";
 import NavBar from "./nav/NavBar"
 
-import "./Cave.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 
 class Cave extends Component {
